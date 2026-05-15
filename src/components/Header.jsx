d7a59301594bc2e76e2653d39b67e0ecd28c228a
zooWorldCode <0706nhj@gmail.com>
2026-05-15 12:40:08 +0900

    hh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
     <header className="site-header">
       <div className="site-header__inner">
         <Link className="site-header__brand" to="/home">
-          현주의 세상
+          현주의 포트폴리오
         </Link>
         <nav aria-label="Primary" className="site-header__nav">
           {navItems.map((item) => (
